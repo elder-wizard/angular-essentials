2e24d53fe47fbf64ea1d69521b46ced774dcf060
Vladyslav Antonov <elder-wizard@ukr.net>
2021-10-17 17:53:37 +0200

    pipe

--- basic-n1/src/app/app.component.ts
+++ basic-n1/src/app/app.component.ts
@@ -8,15 +8,6 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-  toggleStyle: boolean = false;
-  toggleClass: boolean = false;
-  toggleTag: boolean = false;
-
-  constructor() {
-    // setTimeout(() => {
-    //   this.title = 'Second slogan binded';
-    //   this.class = 'secondary';
-    // }, 5000);
-  }
+  now: Date = new Date();
 
 }
